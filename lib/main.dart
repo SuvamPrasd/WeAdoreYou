5c2779203ca962ae0346e6f5fbc51cb5bd14a3eb
suvam prasad <tituprasad30@gmail.com>
2020-10-21 13:36:24 +0530

    start-template

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'We Adore You',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('We Adore You'),
+      ),
+    );
+  }
+}
---
+++ lib/utilities/details.dart
@@ -0,0 +1,4 @@
+const String first_desc =
+    "Welcome Here. In this App, you can find the details related to females' puberty, periods/menstruation, health issues, and more. You will get enough information about your body so that you will be dependent on others.";
+
+const String main_title = "We Adore You";
