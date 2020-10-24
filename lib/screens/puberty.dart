420422cfb320c0a20d77e837fc7708d915d0ba68
suvam prasad <tituprasad30@gmail.com>
2020-10-24 14:12:04 +0530

    [added]: puberty main screen

--- lib/screens/puberty.dart
+++ lib/screens/puberty.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
+import '../utilities/details.dart';
 
 class Puberty extends StatelessWidget {
   static const routeName = '/puberty';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    double _deviceWidth = MediaQuery.of(context).size.width;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Puberty',
+            style: TextStyle(fontSize: 16, color: Colors.black),
+          ),
+          iconTheme: IconThemeData(color: Colors.black),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            width: _deviceWidth,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  Image(
+                    image: AssetImage(
+                      'assets/images/puberty1.jpg',
+                    ),
+                    fit: BoxFit.contain,
+                    alignment: Alignment.center,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    about_puberty,
+                    style: TextStyle(
+                      height: 2.0,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Influential factors for female puberty starts quickly',
+                    style: TextStyle(fontSize: 21),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SingleChildScrollView(
+                    child: ListView.builder(
+                      itemBuilder: (ctx, index) {
+                        return Text(
+                          puberty_factors[index],
+                          style: TextStyle(height: 2.0, color: Colors.grey),
+                        );
+                      },
+                      itemCount: puberty_factors.length,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/utilities/details.dart
+++ lib/utilities/details.dart
@@ -2,3 +2,13 @@ const String first_desc =
     "Welcome Here. In this App, you can find the details related to females' puberty, periods/menstruation, health issues, and more. You will get enough information about your body so that you will be dependent on others.";
 
 const String main_title = "We Adore You";
+
+const String about_puberty =
+    "Puberty is a sequence of events in which physical changes occur, resulting in adult physical characteristics and capacity to reproduce. These physical changes are regulated by changes in the levels of hormones that are produced by the pituitary gland—luteinizing hormone and follicle-stimulating hormone.";
+
+const List puberty_factors = [
+  'Good nutrition and general health.',
+  'Puberty tends to start earlier in girls who are slightly overweight.',
+  'Puberty occurs earlier in girls whose mothers matured early.',
+  'Puberty tends to start earlier in blacks and Hispanics than in Asians and whites who are not Hispanic.'
+];
