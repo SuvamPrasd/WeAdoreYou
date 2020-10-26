6c6fce0621fd7cdca7340df325950bd3195fe52e
suvam prasad <tituprasad30@gmail.com>
2020-10-26 14:42:54 +0530

    [added]: mens page route

--- lib/screens/mens.dart
+++ lib/screens/mens.dart
@@ -1,13 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:slimy_card/slimy_card.dart';
+
+import '../utilities/details.dart';
 
 class Mens extends StatelessWidget {
   static const routeName = '/mens';
 
   @override
   Widget build(BuildContext context) {
+    double _deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(),
-      
+      appBar: AppBar(
+        title: Text(
+          'Menstruation',
+          style: TextStyle(fontSize: 16, color: Colors.black),
+        ),
+        iconTheme: IconThemeData(color: Colors.black),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          width: _deviceWidth,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage(
+                    'assets/images/period.jpg',
+                  ),
+                  fit: BoxFit.contain,
+                  alignment: Alignment.center,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  periods,
+                  style: TextStyle(
+                    height: 2.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ListView.builder(
+                  itemBuilder: (ctx, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SlimyCard(
+                        width: 300,
+                        color: Colors.red[200],
+                        topCardHeight: 200,
+                        bottomCardHeight: 250,
+                        slimeEnabled: true,
+                        topCardWidget: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            periods_que[index]["que"],
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        bottomCardWidget: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(
+                            periods_que[index]["ans"],
+                            style: TextStyle(color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: periods_que.length,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  physics: ScrollPhysics(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/utilities/details.dart
+++ lib/utilities/details.dart
@@ -28,27 +28,64 @@ const List<String> puberty_emotion = [
 //information about periods/menstruation
 const String periods =
     "A period is a release of blood from a girl's uterus, out through her vagina. It is a sign that she is getting close to the end of puberty.";
-const String periods_ans =
-    "Most girls get their first period when they're around 12. But getting it any time between age 10 and 15 is OK. Every girl's body has its own schedule.";
-const String periods_ans2 =
-    "A period happens because of changes in hormones in the body. Hormones are chemical messengers. The ovaries release the female hormones estrogen and progesterone .";
-const String periods_ans3 =
-    "Ovulation is the release of an egg from the ovaries. The same hormones that cause the uterus lining to build up also cause an egg to leave one of the ovaries. The egg travels through a thin tube called a fallopian tube to the uterus.";
-const String periods_ans4 =
-    "For the first few years after a girl starts her period, it may not come regularly. This is normal at first. By about 2–3 years after her first period, a girl's periods should be coming around once every 4–5 weeks.";
-const String periods_ans5 =
-    "Yes, a girl can get pregnant as soon as her period starts. A girl can even get pregnant right before her very first period. This is because a girl's hormones might already be active.";
-const String periods_ans6 =
-    "Periods usually last about 5 days. But a period can be shorter or last longer.";
-const String periods_ans7 =
-    "Periods usually happen about once every 4–5 weeks. But some girls get their periods a little less or more often.";
-const String periods_ans8 =
-    "It may look like a lot of blood, but a girl usually only loses a few tablespoons of blood during the whole period. Most girls need to change their pad, tampon, or menstrual cup about 3‒6 times a day.";
-const String periods_ans9 =
-    "When women reach menopause (around age 45‒55), their periods will permanently stop. Women also won't have a period while they are pregnant.";
-const String periods_ans10 =
-    "PMS (premenstrual syndrome) is when a girl has emotional and physical symptoms that happen before or during her period. These symptoms can include moodiness, sadness, anxiety, bloating, and acne. The symptoms go away after the first few days of a period.";
-const String periods_ans11 =
-    "A warm heating pad on your belly, and taking ibuprofen (Advil, Motrin, or store brand) or naproxen (Aleve or store brand)";
-const String periods_ans12 =
+
+const List<dynamic> periods_que = [
+  {
+    "que": "When Do Most Girls Get Their Period?",
+    "ans":
+        "Most girls get their first period when they're around 12. But getting it any time between age 10 and 15 is OK. Every girl's body has its own schedule.",
+  },
+  {
+    "que": "What Causes a Period?",
+    "ans":
+        "A period happens because of changes in hormones in the body. Hormones are chemical messengers. The ovaries release the female hormones estrogen and progesterone .",
+  },
+  {
+    "que": "How Does Ovulation Relate to Periods?",
+    "ans":
+        "Ovulation is the release of an egg from the ovaries. The same hormones that cause the uterus lining to build up also cause an egg to leave one of the ovaries. The egg travels through a thin tube called a fallopian tube to the uterus.",
+  },
+  {
+    "que": "Do Periods Happen Regularly When Menstruation Starts?",
+    "ans":
+        "For the first few years after a girl starts her period, it may not come regularly. This is normal at first. By about 2–3 years after her first period, a girl's periods should be coming around once every 4–5 weeks.",
+  },
+  {
+    "que": "Can a Girl Get Pregnant as Soon as Her Period Starts?",
+    "ans":
+        "Yes, a girl can get pregnant as soon as her period starts. A girl can even get pregnant right before her very first period. This is because a girl's hormones might already be active.",
+  },
+  {
+    "que": "How Long Do Periods Last?",
+    "ans":
+        "Periods usually last about 5 days. But a period can be shorter or last longer.",
+  },
+  {
+    "que": "How Often Does a Period Happen?",
+    "ans":
+        "Periods usually happen about once every 4–5 weeks. But some girls get their periods a little less or more often.",
+  },
+  {
+    "que": "How Much Blood Comes Out?",
+    "ans":
+        "It may look like a lot of blood, but a girl usually only loses a few tablespoons of blood during the whole period. Most girls need to change their pad, tampon, or menstrual cup about 3‒6 times a day.",
+  },
+  {
+    "que": "Will I Have Periods for the Rest of My Life?",
+    "ans":
+        "When women reach menopause (around age 45‒55), their periods will permanently stop. Women also won't have a period while they are pregnant.",
+  },
+  {
+    "que": "What Is PMS?",
+    "ans":
+        "PMS (premenstrual syndrome) is when a girl has emotional and physical symptoms that happen before or during her period. These symptoms can include moodiness, sadness, anxiety, bloating, and acne. The symptoms go away after the first few days of a period.",
+  },
+  {
+    "que": "What Can I Do About Cramps?",
+    "ans":
+        "A warm heating pad on your belly, and taking ibuprofen (Advil, Motrin, or store brand) or naproxen (Aleve or store brand)",
+  }
+];
+
+const String periods_words =
     "Periods are a natural, healthy part of a girl's life. They shouldn't get in the way of exercising, having fun, and enjoying life.";
